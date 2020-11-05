731b98800e4b16a881446329425cda741ff3f617
anhnguyen-agilityio <anh.nguyenkim@asnet.com.vn>
2020-11-06 00:30:47 +0700

    Initial Project

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookServices;
+
+        public BooksController(BookService bookService)
+        {
+            _bookServices = bookService;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult<List<Book>> Get() => _bookServices.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookServices.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookServices.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookServices.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookServices.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookServices.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookServices.Remove(book.Id);
+
+            return NoContent();
+        }
+
+    }
+}
